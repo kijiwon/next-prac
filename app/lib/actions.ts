a2762c08a701b1dc71330b163478d776c375a313
kijiwon <kiji1313@gmail.com>
2024-05-10 20:13:03 +0900

    invoice 업데이트

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,10 +13,11 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customId'),
+    customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
@@ -33,5 +34,24 @@ export async function createInvoice(formData: FormData) {
   //     status: formData.get('status'),
   //   };
   revalidatePath('dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+  UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+  WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
