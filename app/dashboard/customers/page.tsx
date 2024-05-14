387103ec318b0d9e302f3ba75a77b1b51d2d2833
kijiwon <kiji1313@gmail.com>
2024-05-14 17:06:46 +0900

    customers 데이터 요청

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page() {
+  const allCustomers = await fetchFilteredCustomers('');
+
+  return (
+    <p>
+      <CustomersTable customers={allCustomers} />
+    </p>
+  );
 }
